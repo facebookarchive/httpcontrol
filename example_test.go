a766f66fcc33f78d0156dc2137e9cf60c0bb4d0a
Naitik Shah <n@daaku.org>
2013-09-13 18:46:14 -0700

    remove Close from example since it isn't strictly necessary

--- example_test.go
+++ example_test.go
@@ -11,16 +11,14 @@ import (
 )
 
 func Example() {
-	// This example doesn't use all available features, look at the API for
-	// documentation on all options.
-	transport := &httpcontrol.Transport{
-		RequestTimeout: time.Minute,
-		MaxTries:       3,
+	// The provided Transport can be used via the HTTP client or used directly
+	// via RoundTrip.
+	client := &http.Client{
+		Transport: &httpcontrol.Transport{
+			RequestTimeout: time.Minute,
+			MaxTries:       3,
+		},
 	}
-
-	// It can then be used as the Transport in a HTTP client (or used directly
-	// via RoundTrip).
-	client := &http.Client{Transport: transport}
 
 	res, err := client.Get("http://graph.facebook.com/naitik?fields=name")
 	if err != nil {
@@ -31,13 +29,6 @@ func Example() {
 
 	// Just outputting the response for example purposes.
 	if _, err := io.Copy(os.Stdout, res.Body); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	// This isn't strictly necessary, but it will shutdown the background
-	// goroutine monitoring for request timeouts.
-	if err := transport.Close(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
